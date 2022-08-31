b9456257fb3b9e64405506faa686b6be700e9d28
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-31 15:47:59 +0530

    Added:Ability For The Greeting App To Give Greeting Message

--- src/main/java/com/bridgelabz/greetingappdevlopment/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingappdevlopment/service/GreetingService.java
@@ -3,7 +3,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+
     public String getMessage() {
         return "Hello World";
+    }
+
+    public String getGreetingMessage(String firstName, String lastName) {
+        return "Hello " + firstName + "" + lastName;
+    }
+
+    public String postMessage(String firstName, String lastName) {
+        return "Hello " + firstName + "" + lastName;
     }
 }
