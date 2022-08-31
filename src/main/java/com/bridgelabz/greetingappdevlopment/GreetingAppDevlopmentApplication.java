60af587307e895a9c1fc2149567a74b1c4ece343
rushi1410 <mr.rushikesh.molkire@outlook.com>
2022-08-31 13:54:55 +0530

    Added:Welcome To Greeting App Devlopment

---
+++ src/main/java/com/bridgelabz/greetingappdevlopment/GreetingAppDevlopmentApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.greetingappdevlopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevlopmentApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Greeting App Devlopment");
+        SpringApplication.run(GreetingAppDevlopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/greetingappdevlopment/GreetingAppDevlopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappdevlopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppDevlopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
